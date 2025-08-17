5ddcac37825fff07b171602858c706e05714a7dd
DESKTOP-KEHG5MG\danie <danieljlm19951@gmail.com>
2025-08-17 12:36:34 -0500

    Curso error message 07/08/202512:36

--- src/modules/Registro/Views/Registroview.vue
+++ src/modules/Registro/Views/Registroview.vue
@@ -1,16 +1,35 @@
 <template>
     <div>
         <h2>Formulario De registro</h2>
-        <form action="">
-            <label for="nombre">nombre</label>
-            <input type="text" id="nombre" placeholder="ingrese su nombre">
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="ingrese su email">
-            <button type="submit">Registrar</button>
-        </form>
+        <Form :validation-schema="schema" @submit="onSubmit">
+            <div class="Form">
+                <label for="nombre">nombre</label>
+                <Field type="text" id="nombre" name="nombre" placeholder="ingrese su nombre" />
+                <error-message name="nombre"></error-message>
+            </div>
+            <div class="Form">
+                <label for="email">Email</label>
+                <Field type="email" id="email" name="email" placeholder="ingrese su email" />
+                <error-message name="email"></error-message>
+            </div>
+            <div class="Form">
+                <button type="submit">Registrar</button>
+            </div>
+
+        </Form>
     </div>
 </template>
-<script setup>
 
+<script setup>
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import { schema } from '../Schemas/validationSchema';
+const onSubmit = () => {
+    console.log('Se ha enviado el formulario correctamente')
+}
 </script>
-<style scoped></style>
+
+<style scoped>
+.Form {
+    margin-bottom: 10px;
+}
+</style>
