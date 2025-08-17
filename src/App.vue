964e4c4958d6f2198c3be2cd54999a2885e71a74
DESKTOP-KEHG5MG\danie <danieljlm19951@gmail.com>
2025-08-17 09:21:47 -0500

    Primeros Cambios curo Vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+
+<template>
+  
+  <contador></contador>
+   
+  
+</template>
+<script setup>
+
+  import contador from './components/contador.vue';
+
+</script>
+<style scoped></style>
---
+++ src/components/contador.vue
@@ -0,0 +1,25 @@
+<template>
+
+<p>{{numero}}</p>
+<button @click="aumentarNumero">Aumentame</button>
+<button @click="Disminuyeme">Disminuyeme</button>
+</template>
+<script setup>
+
+import {ref} from 'vue';
+
+const numero = ref(0);
+const aumentarNumero = () =>
+{
+    numero.value++;
+}
+const Disminuyeme = () =>
+{
+    numero.value--;
+}
+
+</script>
+<style>
+
+
+</style>
