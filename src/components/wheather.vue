95911d26f651163b6a7534f1cd814e2b032d1999
DESKTOP-KEHG5MG\danie <danieljlm19951@gmail.com>
2025-08-18 10:56:02 -0500

    agregar componente API

--- src/components/wheather.vue
+++ src/components/wheather.vue
@@ -1,7 +1,20 @@
 <template>
-  7°
+  {{ weatherStore.temperatura }}
 </template>
+
 <script setup>
+import { useWeather } from '../compusables/useWeather';
+import { onMounted } from 'vue';
+import { useWeatherStore } from '../stores/weatherStore';
+
+const weatherStore = useWeatherStore();
+
+onMounted(()=>{
+  useWeather();
+})
 
 </script>
-<style scoped></style>
+
+<style scoped>
+
+</style>
--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -3,9 +3,12 @@ import axios from "axios";
 
 const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=4.6097&longitude=-74.0817&hourly=temperature_2m&timezone=auto';
 
-export const getTempreatura = async ()=>
-{
-   const axiosWeatherResponse = await axios.get(API_URL);
-   const nuevaTempratura = axiosWeatherResponse.hourly.temperature_2m[0];
-   return nuevaTempratura;
-}
+export const getTemperatura = async () =>{
+   const respuesta = await axios.get(API_URL);
+   const naranjas = respuesta.filter(item => item.name === 'Naranja');
+   const nuevaTemperatura = respuesta.data.hourly.temperature_2m;
+   
+   const nuevaTemperaturagrados = respuesta.data.hourly_units.temperature_2m[0];
+   const primeratemp = nuevaTemperatura[0] + " " + nuevaTemperaturagrados;
+   return primeratemp;
+};
